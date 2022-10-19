ac552275868c5d6a439d40e1a69bdb9e1f3737dc
DmitriiYaganin <dmitrij.yaganin@mail.ru>
2022-10-19 16:20:43 +0300

    Creat HW 21

---
+++ entity/shop.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+from entity.base_storage import BaseStorage
+from exceptions import TooManyDifferentProductsError
+
+
+class Shop(BaseStorage):
+
+    def __init__(self, items: Dict[str, int], capasity: int, max_unique_items: int):
+        self.__max_unique_items = max_unique_items
+        super().__init__(items, capasity)
+
+    def add(self, name: str, amount: int) -> None:
+        if self.get_unique_items_count() >= self.__max_unique_items:
+            raise TooManyDifferentProductsError
+
+        super().add(name, amount)
